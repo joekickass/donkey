c1f5751d4efdab3f9d8a4de374b7177979df9057
Tomas <tomas.nilsson@jayway.com>
2018-07-04 05:46:27 +0200

    Add 2 training tests

--- donkeycar/tests/test_keras.py
+++ donkeycar/tests/test_keras.py
@@ -31,6 +31,31 @@ def test_train_model(models_dir, tubs_dir, pilot):
     y = ['angle', 'throttle']
     train_gen, val_gen = tub.get_train_val_gen(x, y)
 
-    ret = pilot.train(train_gen, val_gen, model_path, epochs=1)
+    hist = pilot.train(train_gen, val_gen, model_path, epochs=1)
 
-    assert ret is not None
+    nbr_if_epochs = len(hist.history['loss'])
+
+    assert nbr_if_epochs == 1
+
+
+def test_train_model_2(models_dir, tubs_dir, pilot):
+    """ Test train model for 2 epochs and ensure it improves """
+    # Create a tub with 100 records
+    tub = create_sample_tubs(tubs_dir, cnt=1, records=100)[0]
+
+    # Create path to model
+    model_path = os.path.join(models_dir, 'test-train-model')
+
+    # Create generators
+    x = ['cam/image_array']
+    y = ['angle', 'throttle']
+    train_gen, val_gen = tub.get_train_val_gen(x, y)
+
+    hist = pilot.train(train_gen, val_gen, model_path, epochs=2)
+
+    nbr_if_epochs = len(hist.history['loss'])
+    # Check that we ran for 2 epochs
+    assert nbr_if_epochs == 2
+    # Make sure error (loss) decreased after epoch 2. This is actually not guaranteed,
+    # but the CNN and training data must be very bad if not.
+    assert hist.history['loss'][0] > hist.history['loss'][1]
